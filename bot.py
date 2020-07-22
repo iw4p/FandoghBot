af38ceba8b8002a59462d739649f3c6cdd8cf2a9
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-07-23 02:39:52 +0430

    /login

--- bot.py
+++ bot.py
@@ -1,35 +1,106 @@
-# Imports
-import sys
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-import logging
+#Start
+#Importing Modules
+try:
+    import sys
+    import time
+    from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+    from telegram.ext import MessageHandler, Filters, Updater, CommandHandler, CallbackQueryHandler
+    import logging
+except ImportError as e:
+	print("Problem: ",e)
+	exit()
 
+login_post = {}
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class LoginInfo:
+        email = ''
+        password = ''
+
+
+credentials = LoginInfo()
+
+
+
 
 def start(update, context):
-    keyboard = [[InlineKeyboardButton("Login to Fandogh", callback_data='1')]]
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
+        hour = int(time.strftime("%H"))
 
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+        text = ''
+
+        credentials.email = ''
+        credentials.password = ''
+
+        if hour >= 0 and hour <= 11:
+                text="Good Morning!"
+
+        elif hour >= 12 and hour <= 18:
+                text="Good Afternoon!"
+
+        elif hour >= 17 and hour <= 23:
+                text="Good Evening!"
+
+        keyboard = [[InlineKeyboardButton("Login", callback_data='1')]]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        update.message.reply_text(text)
+
+        update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 
-def button(update, context):
-    query = update.callback_query
 
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    query.answer()
 
-    query.edit_message_text(text="Selected option: {}".format(query.data))
+# def login(bot, update, args):
+#     if len(args) > 1:
+#         username = args[0]
+#         password = args[1]
+#         global login_post
+#         login_post = {
+#             'username': username.lower(),
+#             'password': password
+#         }
+#         bot.sendMessage(chat_id=update.message.chat_id, text="Now you can send private url of photo or video of your private friends.")
+#         print(username, password)
+#     else:
+#         bot.sendMessage(chat_id=update.message.chat_id, text="Try again. Use /login username password.")
 
+def login(update, context):
+    """Command handler: /login <username> <password> - save login to database."""
+    logging.info(f"Command: /login triggered by {update.effective_chat.id}")
+    if len(context.args) != 2:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Not enough or too many parameters, I need both usernamae and password.",
+        )
+    else:
+        print(context.args)
+
+                        
+
+
+# def loginButton(update, context):
+#     query = update.callback_query
+
+#     query.answer()
+
+#     query.edit_message_text(text="Enter your username: ")
+
+def getUserText(update, context):
+    text = update.message.text
+    print(text)
+    return text
 
 def help_command(update, context):
-    update.message.reply_text("Use /start to test this bot.")
+    update.message.reply_text("Use /start to login again.")
+
+def getToken():
+    # api.fandogh.cloud/api/tokens
+    print("get token")
 
 
 def main():
@@ -38,13 +109,20 @@ def main():
     tokenParam = sys.argv[1] 
 
     # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
     updater = Updater(tokenParam, use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    dp = updater.dispatcher
+
+
+    dp.add_handler(CommandHandler('start', start))
+    # dp.add_handler(CallbackQueryHandler(loginButton))
+    # dp.add_handler(MessageHandler(Filters.text, getUserText))
+
+    login_handler = CommandHandler('login', login, pass_args=True)
+    dp.add_handler(login_handler)
+
+
+    dp.add_handler(CommandHandler('help', help_command))
 
     # Start the Bot
     updater.start_polling()
